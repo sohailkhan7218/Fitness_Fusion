795c34eb2f0d8bea2691d7db1588941729ba174e
sohail khan <khansohail7218@gmail.com>
2024-09-20 17:32:03 +0530

    first version of fitness fusion

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="container">This is the first website using php </div>
+
+
+
+    <?php
+        define('pi',3.14);
+        echo "Fitness Fusion";
+
+        $variable1 = 45;
+        $variable2 = 55;
+        echo "the sum of variable1 and variable2 is";
+        echo "<br>";
+        echo $variable1+$variable2;
+        echo "<br>";
+        $newvar ="sohail khan";
+        echo var_dump($newvar);
+        echo "<br>";
+        echo var_dump($variable1);
+
+        echo pi;
+    ?>
+
+
+
+</body>
+</html>
